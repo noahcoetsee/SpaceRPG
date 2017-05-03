62ebd52539231d7feb7c1e98605ce88e30e58daf
Noah <noahcoetsee@gmail.com>
2017-05-02 21:09:16 -0400

    Add contents

--- root/server.js
+++ root/server.js
@@ -1,1 +1,38 @@
+//
+// # Main Server
+//
+// This server will handle all of the main synchronization of variables, including positions, health, etc.
+//
+var http = require('http');
+var path = require('path');
 
+var socketio = require('socket.io');
+var express = require('express');
+
+//
+// ## Create the server using express, with a static output (for now).
+//
+// Creates a new instance of the Router with the following options:
+//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
+//
+var router = express();
+var server = http.createServer(router);
+var io = socketio.listen(server);
+
+router.use(express.static(path.resolve(__dirname, 'client')));
+var sockets = [];
+
+io.on('connection', function (socket) {
+  sockets.push(socket);
+});
+
+function broadcast(event, data) {
+  sockets.forEach(function (socket) {
+    socket.emit(event, data);
+  });
+}
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Main SpaceRPG Server listening at", addr.address + ":" + addr.port);
+});
