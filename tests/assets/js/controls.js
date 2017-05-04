670be41563e5dd4033557a7f9884a086dc0f7721
Noah <noahcoetsee@gmail.com>
2017-05-04 15:11:08 -0400

    Finished basic controls [TESTS]

--- tests/assets/js/controls.js
+++ tests/assets/js/controls.js
@@ -1,1 +1,50 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, "SpaceRPG Test: Controls", { preload: preload, create: create, update: update });
 
+// Variables
+var space;
+var player;
+var turret;
+
+// Preload
+function preload() {
+  // Game Scaling
+  game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  game.scale.pageAlignHorizontally = true;
+  game.scale.pageAlignVertically = true;
+  
+  game.load.image('space', '../assets/images/deepspace.jpg');
+  game.load.image('frigate', '../assets/images/Human-Frigate.png');
+  game.load.image('turret', '../assets/images/turret.png');
+}
+
+// Create
+function create() {
+  space = game.add.tileSprite(game.world.centerX, game.world.centerY, 5000, 5000, 'space');
+  
+  game.world.setBounds(0, 0, 5000, 5000);
+  
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  
+  player = game.add.sprite(game.world.centerX, game.world.centerY, 'frigate');
+  player.anchor.setTo(0.5, 0.5);
+  game.physics.arcade.enable(player);
+  player.body.angularDrag = 50;
+  player.body.maxAngular = 500;
+  
+}
+
+// Update
+function update() {
+  
+  if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+    sprite.body.angularVelocity = -200;
+  }
+  
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+      sprite.body.angularVelocity = 200;
+    }
+  
+  if(game.input.keyboard.isDown(Phaser.Keyboard.UP) {
+     game.physics.arcade.velocityFromAngle(player.angle, 300, player.body.velocity);
+  }
+}
